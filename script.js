9525796c5136a69ce1b125e8e81767604578c55a
Victor Hugo Mota Medeiros <vh.mota.med>
2025-09-24 21:45:24 -0300

    Revert "feat: Adiciona interatividade ao botão de gerar conselho"
    This reverts commit 79ec6d5599519c4120e6905ddb42a840d75a0509.

--- script.js
+++ script.js
@@ -3,12 +3,4 @@ const conselhos = [
     "A persistência realiza o impossivel.", "Aprenda algo novo hoje.",
     "Seja gentil com todos que encontrar."
 ]
-const conselhoTexto = document.getElementById('conselhoTexto');
-const novoConselhoBtn = document.getElementById('novoConselhoBtn');
-
-function gerarConselho() {
-    //BUG INTENCIONAL AQUI
-    const indice = Math.floor(Math.random() * (conselhos.length + 1));
-    conselhoTexto.textContent = conselhos[indice];
-}
-novoConselhoBtn.addEventListener('click', gerarConselho);
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
