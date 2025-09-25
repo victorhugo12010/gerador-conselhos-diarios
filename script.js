1ba2be774605796cdc37b27ab20fee1bf29dda52
Victor Hugo Mota Medeiros <vh.mota.med>
2025-09-24 21:20:47 -0300

    Criação e upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+const conselhos = [
+    "Acredite em você mesmo.", "O primero passo é o mais importante.",
+    "A persistência realiza o impossivel.", "Aprenda algo novo hoje.",
+    "Seja gentil com todos que encontrar."
+]
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
